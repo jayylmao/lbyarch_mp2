8ec2b1793ce795b8e47294d4022b64da730ebe72
jayylmao <73204320+jayylmao@users.noreply.github.com>
2024-12-01 06:36:37 +0800

    fix: asm code optimization

--- asmDotProduct.asm
+++ asmDotProduct.asm
@@ -1,7 +1,4 @@
 ; Jay Michael Carlos and Janica Megan Reyes - LBYARCH S14
-section .data
-	one	dd	1.0										; temporary variable to return 1.
-	msg	db	"rdi: %d", 10, 0
 section .text
 bits 64												; define project as 64-bit.
 default rel
@@ -17,18 +14,19 @@ asmDotProduct;
 	; r8: address of vector b.
 	sub		rsp, 40									; allot shadow space on stack for arguments.
 	xorps	xmm0, xmm0								; set xmm0 to 0 be used as accumulator.
+	mov		rbx, 4									; for optimization, move constant 4 to rbx, as we increment by 4 later on.
 
 loopStart:
 	cmp		rcx, 0									; if all elements have been processed, stop loop.
 	jz		end
 
-	movss	xmm1, dword	[rdx]						; load float a[i] into xmm1
-	movss	xmm2, dword	[r8]						; load float b[i] into xmm2
+	movss	xmm1, [rdx]								; load float a[i] into xmm1
+	movss	xmm2, [r8]								; load float b[i] into xmm2
 	mulss	xmm1, xmm2								; multiply a[i] * b[i]
 	addss	xmm0, xmm1								; add result to accumulator.
 
-	add		rdx, 4									; move to next element in a.
-	add		r8, 4									; move to next element in b.
+	add		rdx, rbx								; move to next element in a.
+	add		r8, rbx									; move to next element in b.
 	dec		rcx										; restart loop if rcx is not 0.
 	jmp		loopStart
 end:
